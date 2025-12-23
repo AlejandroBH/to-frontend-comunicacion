0a4040e5082aa27684ce26c6f390efd9aa97fb20
Alejandro Barrera <johndoe@example.com>
2025-12-22 23:40:30 -0300

    feat: evita que se puedan registrar varios usuarios con el mismo email

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,8 +1,12 @@
 import * as userService from "../services/userService.js";
 
 export const registerUser = async (req, res) => {
-  const data = req.body;
-  const resp = await userService.registerUser(data);
+  try {
+    const data = req.body;
+    const resp = await userService.registerUser(data);
 
-  res.status(201).send(resp);
+    res.status(201).send(resp);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
--- Server/services/userService.js
+++ Server/services/userService.js
@@ -2,6 +2,10 @@ import { users } from "../data/users.js";
 import bcrypt from "bcrypt";
 
 export const registerUser = async (userData) => {
+  const existingUser = users.find((u) => u.email === userData.email);
+  if (existingUser) {
+    throw new Error("El email ya está en uso");
+  }
   const hashedPassword = await bcrypt.hash(userData.password, 10);
   const newUser = { id: Date.now(), ...userData, password: hashedPassword };
   users.push(newUser);
